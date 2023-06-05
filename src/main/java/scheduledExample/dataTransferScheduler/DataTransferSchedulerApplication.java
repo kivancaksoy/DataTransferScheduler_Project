4ea55ee3c4e615f63e9ba8f78b005bcba3b93174
kivancaksoy <kvnc96@hotmail.com>
2023-06-05 14:21:36 +0300

    initial commit

---
+++ src/main/java/scheduledExample/dataTransferScheduler/DataTransferSchedulerApplication.java
@@ -0,0 +1,13 @@
+package scheduledExample.dataTransferScheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataTransferSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataTransferSchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/scheduledExample/dataTransferScheduler/DataTransferSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package scheduledExample.dataTransferScheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataTransferSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
