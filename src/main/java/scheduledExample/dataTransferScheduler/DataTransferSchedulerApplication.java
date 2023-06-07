fa038b88f88e63933501ccdbe8cf1541fa96a131
kivancaksoy <kvnc96@hotmail.com>
2023-06-07 15:45:55 +0300

    unused method deleted.

--- src/main/java/scheduledExample/dataTransferScheduler/DataTransferSchedulerApplication.java
+++ src/main/java/scheduledExample/dataTransferScheduler/DataTransferSchedulerApplication.java
@@ -3,25 +3,13 @@ package scheduledExample.dataTransferScheduler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import scheduledExample.dataTransferScheduler.utilities.customConfigurations.CustomConfigurationService;
 
 @EnableScheduling
 @SpringBootApplication
 public class DataTransferSchedulerApplication {
 
-	private final CustomConfigurationService customConfigurationService;
-
-	public DataTransferSchedulerApplication(CustomConfigurationService customConfigurationService) {
-		this.customConfigurationService = customConfigurationService;
-	}
-
-	public static void main(String[] args) {
-		SpringApplication.run(DataTransferSchedulerApplication.class, args);
-	}
-
-	public void run() {
-		String cronValue = customConfigurationService.getCronValue();
-		System.out.println("Scheduled cron value: " + cronValue);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DataTransferSchedulerApplication.class, args);
+    }
 
 }
