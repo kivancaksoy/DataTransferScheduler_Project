261a2705208ffa02627eb3769de63b5d85390f21
kivancaksoy <kvnc96@hotmail.com>
2023-06-11 01:30:19 +0300

    environment value added to WebClientHelper and application.properties. Dockerfile modified.

--- Dockerfile
+++ Dockerfile
@@ -5,9 +5,23 @@ COPY .mvn .mvn
 RUN ./mvnw dependency:resolve
 
 COPY src src
+
+ARG DB_URL_ARG="jdbc:postgresql://host.docker.internal:5432/DataTransferDB"
+ARG SCHEDULER_ARG="http://host.docker.internal:9090/v1/baseCustomer/getAllCustomerWithVersion?version="
+
+ENV DB_URL=$DB_URL_ARG
+ENV SCHEDULER_SERVICE_URL=$SCHEDULER_ARG
+
 RUN ./mvnw package
 
 FROM openjdk:17
 WORKDIR datatransfersheduler
 COPY --from=build target/*.jar datatransfersheduler.jar
-ENTRYPOINT ["java", "-jar", "datatransfersheduler.jar", "--spring.datasource.url=jdbc:postgresql://host.docker.internal:5432/DataTransferDB"]
+
+ARG DB_URL_ARG="jdbc:postgresql://host.docker.internal:5432/DataTransferDB"
+ARG SCHEDULER_ARG="http://host.docker.internal:9090/v1/baseCustomer/getAllCustomerWithVersion?version="
+
+ENV DB_URL=$DB_URL_ARG
+ENV SCHEDULER_SERVICE_URL=$SCHEDULER_ARG
+
+ENTRYPOINT ["java", "-jar", "datatransfersheduler.jar"]
--- src/main/java/scheduledExample/dataTransferScheduler/business/services/helpers/WebClientHelper.java
+++ src/main/java/scheduledExample/dataTransferScheduler/business/services/helpers/WebClientHelper.java
@@ -1,24 +1,27 @@
 package scheduledExample.dataTransferScheduler.business.services.helpers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 import scheduledExample.dataTransferScheduler.entities.Customer;
 
 @Component
 public class WebClientHelper {
 
+    @Value("${SCHEDULER_SERVICE_URL:http://localhost:9090/v1/baseCustomer/getAllCustomerWithVersion?version=}")
+    private String SchedulerService_URL;
+
     public Flux<Customer> getAllCustomerWithVersion(int version) {
         String versionStr = String.valueOf(version);
         WebClient webClient = WebClient.create();
-        String url = "http://localhost:9090/v1/baseCustomer/getAllCustomerWithVersion?version=" + version;
+        String url = SchedulerService_URL + version;
 
         return webClient.get()
                 .uri(url)
                 .retrieve()
                 .bodyToFlux(Customer.class);
-                //.bodyToMono(String.class);
+        //.bodyToMono(String.class);
     }
 
 
